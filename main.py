d3304026543a51a637f97edcf96065cea0ccad83
simona.santamaria <simona.santamaria@hotmail.com>
2022-02-22 12:31:24 +0100

    middleware changed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 #from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
@@ -31,14 +31,14 @@ origins = [
     "http://0.0.0.0:3000",
     "https://waver-ryver.netlify.app",
 ]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-)
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=origins,
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#    expose_headers=["*"],
+#)
 
 @app.get("/")
 async def read_root():
